1ac15972c3099b1f01e27891f6c148dc210d5c58
mr_goldenman <vasya.chaukin.97@mail.ru>
2023-11-10 21:15:04 +0500

    task2_release_1

---
+++ backend/task2/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+import vk_api
+import json
+access_token = 'vk1.a.v8xx1NAIwR6aPmWt-oXx5gq8l4VtpTdJFOR7hPGUvxtf9zQCGAhaOX_X7oTz45Kgyu_JTQHHWEeQmLs7AX5VDJEweHrwEg6YTvQ9PIOrAsapmjfbJYwyn__fdXO26ce3Vkb0QTebw5RGmX523gJZO_-sxSRdOaZ0kgdbO2xMUu0gdj8Jsk_8c5Kz7tDCrb30Mu10Jy8PEORm0ezPAqmg9w'
+vk_session = vk_api.VkApi(login="79195876294", token=access_token)
+vk = vk_session.get_api()
+
+def user(request):
+    '''request={
+                "name":"Michman"
+    }'''
+    name = json.loads(request.body)["name"]
+    try:
+        usr = vk.users.get(user_id=name)
+        return JsonResponse(usr)
+    except Exception:
+        return JsonResponse({"message":"something went wrong"})
+
+def group(request):
+    '''request={
+                "name":"meido4ka"
+    }'''
+    name = json.loads(request.body)["name"]
+    try:
+        group = vk.groups.getById(group_id=name)
+        return JsonResponse(group)
+    except Exception:
+        return JsonResponse({"message":"something went wrong"})
